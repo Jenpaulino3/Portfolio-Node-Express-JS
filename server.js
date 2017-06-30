253d7d9218e68ba07064b9c30f40caee95fbac0e
Jennifer Paulino <jenpaulino3@gmail.com>
2017-06-30 03:45:56 -0400

    live

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var exphbs  = require('express-handlebars');
-var app = express()
 var path = require("path");
+var app = express()
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'))
+
+app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
