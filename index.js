96d07ec47ce8d0dfb044a017f66f0d3c09d9f9ac
Jennifer Paulino <jenpaulino3@gmail.com>
2017-06-30 02:55:36 -0400

    live

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var exphbs  = require('express-handlebars');
+var app = express()
+var path = require("path");
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'))
+
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(8000, function () {
+  console.log('Server is running on 8000')
+});
